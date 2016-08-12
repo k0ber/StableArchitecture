bd9245807c68e68d4b1b9ad94ecf66acf2f32d1f
Nik Polyakov <Nikita.Polyakov@noveogroup.com>
2016-08-12 15:13:01 +0700

    remove unused component

--- app/src/main/java/com/nik/noveo/stablearchitecture/App.java
+++ app/src/main/java/com/nik/noveo/stablearchitecture/App.java
@@ -2,15 +2,12 @@ package com.nik.noveo.stablearchitecture;
 
 import android.app.Application;
 
-import com.nik.noveo.stablearchitecture.news.mvp.di.ApplicationComponentMVP;
-import com.nik.noveo.stablearchitecture.news.mvp.di.DaggerApplicationComponentMVP;
 import com.nik.noveo.stablearchitecture.news.mvvi.di.ApplicationComponentMVVI;
 import com.nik.noveo.stablearchitecture.news.mvvi.di.DaggerApplicationComponentMVVI;
 
 
 public class App extends Application {
     private ApplicationComponentMVVI applicationComponentMVVI;
-    private ApplicationComponentMVP applicationComponentMVP;
 
     @Override
     public void onCreate() {
@@ -19,20 +16,10 @@ public class App extends Application {
         applicationComponentMVVI = DaggerApplicationComponentMVVI
                 .builder()
                 .build();
-
-        applicationComponentMVP = DaggerApplicationComponentMVP
-                .builder()
-                .build();
     }
 
     public ApplicationComponentMVVI componentMVVI() {
         return applicationComponentMVVI;
     }
-
-    public ApplicationComponentMVP componentMVP() {
-        return applicationComponentMVP;
-    }
-
-
 
 }
